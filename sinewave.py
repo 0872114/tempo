0a8b10178ac7d85b49883685bae38b7debb8521f
Evgeniy Orlov <evgeniyorlov@MacBook-Air-Evgeniy.local>
2021-11-06 03:54:27 +0300

    removed unnecessary stuff

--- sinewave.py
+++
@@ -1,7 +0,0 @@
-import time
-
-from pysinewave import SineWave
-
-# Create a sine wave, with a starting pitch of 12, and a pitch change speed of 10/second.
-sinewave = SineWave(pitch=24, pitch_per_second=10)
-sinewave.output_stream
